229028141e100ed8e9221e8602e6c5daadf8ad5d
jpmonettas <jpmonettas@gmail.com>
2014-04-03 14:53:47 -0300

    Added jmonetta-pack again

---
+++ jmonetta-pack/config/bindings.el
@@ -0,0 +1,21 @@
+;; Place your bindings here.
+
+;; For example:
+(define-key global-map (kbd "C-+") 'text-scale-increase)
+(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+(global-set-key (kbd "C-.") 'er/expand-region)
+(global-set-key (kbd "C-,") 'er/contract-region)
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-S-l") 'helm-locate)
+(global-set-key (kbd "C-S-n") 'helm-git-grep)
+
+(global-set-key (kbd "<f2>") 'minimap-create)
+(global-set-key (kbd "<f3>") 'minimap-kill)
+
+(global-unset-key (kbd "M-<down-mouse-1>"))
+(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
+
+(global-set-key (kbd "C-x n") 'popup-todos)
